1815743424841f92b0fb305cf1436f355d7e9243
joão Marcelo <joaovictormarcelo5@gmail.com>
2025-03-26 06:50:19 -0300

    usando o git ignore

--- obj/Debug/net9.0/Leetcode.AssemblyInfo.cs
+++ obj/Debug/net9.0/Leetcode.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Leetcode")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76137eef3b4c941b613d37a738717351f9dfd487")]
 [assembly: System.Reflection.AssemblyProductAttribute("Leetcode")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Leetcode")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
