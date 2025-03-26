76137eef3b4c941b613d37a738717351f9dfd487
joão Marcelo <joaovictormarcelo5@gmail.com>
2025-03-26 06:42:49 -0300

    inicializando

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Myprogram;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+     
+        // string manipulator
+        string pattern = @"^.{8,}$";
+        string pattern1 = @"^(?=.*[A-Z])(?=.*[a-z])(?=.*\d).{8,}$";
+        string pattern2 = @"^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[!@#$%])[A-Za-z/d!@#$%].{8,}$";
+        string password = "aice00024@#!";
+        
+        // if the string matches the pattern, returns true, otherwise return false
+        var result = Regex.IsMatch(password, pattern2);
+        
+        Console.WriteLine(result);
+
+      
+    }
+
+ 
+    
+}
